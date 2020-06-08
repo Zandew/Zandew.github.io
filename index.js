8b0a489040b00ecfad1b4bbf7e49de09722a0756
Andrew Xue <andewzxue@gmail.com>
2020-06-08 18:17:51 -0400

    f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+function about() {
+    Document.getElementById("info").innerHTML = "<h1> ABOUT </h1>";
+}
+
+function solutions() {
+    Document.getElementById("info").innerHTML = "<h1> SOLUTIONS </h1>";
+}
