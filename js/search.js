554fa9f229f51dd519efc781fd4e84a3ab1999b5
Andrew Xue <andewzxue@gmail.com>
2020-06-10 07:53:35 -0400

    ff

--- js/search.js
+++ js/search.js
@@ -28,14 +28,12 @@
              }
          }
      }
-
+     
      var searchTerm = document.getElementById('search-box').value;//getQueryVariable('query');
      
      if (searchTerm) {
          document.getElementById('search-box').setAttribute("value", searchTerm);
 
-         // Initalize lunr with the fields it will be searching on. I've given title
-         // a boost of 10 to indicate matches on this field are more important.
          var idx = lunr(function () {
              this.field('id');
              this.field('title', { boost: 10 });
@@ -48,8 +46,8 @@
                      'url': window.store[key].url
                  });
              });
-             var results = idx.search(searchTerm);
-             displaySearchResults(results, window.store);
          }
+        var results = idx.search(searchTerm);
+        displaySearchResults(results, window.store);
     }
  })();
