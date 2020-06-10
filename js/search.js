d640e9964f657cc9a4742698fb5b2b3292939ed1
Andrew Xue <andewzxue@gmail.com>
2020-06-10 08:05:28 -0400

    f

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
- (function() {
-     document.getElementById('search-results').innerHTML = "<li>HELLO</li>";
+document.getElementById('search-results').innerHTML = "<li>HELLO</li>";
+(function() {
      /*
      function displaySearchResults(results, store) {
          var searchResults = document.getElementById('search-results');
