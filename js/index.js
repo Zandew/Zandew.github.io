2202d90f03aafd07fd5f5f61798bad596aacbc89
Andrew Xue <andewzxue@gmail.com>
2020-06-11 19:35:34 -0400

    f

--- js/index.js
+++ js/index.js
@@ -15,6 +15,11 @@ function solutions() {
 
 function find(){
     var prob = document.getElementById("search-box").value.trim().toLowerCase();
+    {% for post in site.posts %}
+        {% if post.title == prob %}
+            document.getElementById("code").innerHTML = '<div id="code-ctn"><pre class="prettyprint lang-cpp" style="float: left">'+window.store[key].content+'</pre></div>';
+    {% endif %}
+    {% endfor %}
     for (var key in window.store){
         if (window.store[key].title.toLowerCase() == prob){
             document.getElementById("code").innerHTML = '<div id="code-ctn"><pre class="prettyprint lang-cpp" style="float: left">'+window.store[key].content+'</pre></div>';
