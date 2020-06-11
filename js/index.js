6d20bdac13b8bc3eeb7a401c7b46be2c8b343fb0
Andrew Xue <andewzxue@gmail.com>
2020-06-11 17:40:58 -0400

    f

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 
 function about() {
-    document.getElementById("info").innerHTML = '<div class="me" style="width: 60%; height: 200px; display: inline-block;"> <img class="andrewxue" src="./andrewxue.png" alt="Andrew Xue" style="width: 25%; height: 200px; float: left;"> <p style="margin-left: 25%; height: 200px; font-family: \'Balsamiq Sans\'">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> </div>';
+    document.getElementById("info").innerHTML = '<div id="about"> <img class="andrewxue" src="./andrewxue.png" alt="Andrew Xue" id="me"> <p id="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> </div>';
     document.getElementById("code").innerHTML = "";
     document.getElementById("btn1").style.color = "black";
     document.getElementById("btn2").style.color = "gray";
 }
 
 function solutions() {
-    document.getElementById("info").innerHTML = '<input type="text" placeholder="Find a Problem" id="search-box" style="outline: none; width: 200px; height: 25px; display: inline-block; vertical-align: top;"><input type="image" src="/imgs/search.png" onclick="find()" style="width: 30px; height: 30px; outline: none; margin-left:5px">';
+    document.getElementById("info").innerHTML = '<input type="text" placeholder="Find a Problem" id="search-box"><input type="image" src="/imgs/search.png" id="search-btn" onclick="find()">';
     document.getElementById("code").innerHTML = '<h2 id="tip"> Enter a DMOJ or Codeforces problem code (e.g. ccc20s5 or 1234A) </h2>';
     document.getElementById("btn1").style.color = "gray";
     document.getElementById("btn2").style.color = "black";
@@ -17,7 +17,7 @@ function find(){
     var prob = document.getElementById("search-box").value.trim().toLowerCase();
     for (var key in window.store){
         if (window.store[key].title.toLowerCase() == prob){
-            document.getElementById("code").innerHTML = '<pre class="prettyprint lang-cpp">'+window.store[key].content+'</pre>';
+            document.getElementById("code").innerHTML = '<div><pre class="prettyprint lang-cpp">'+window.store[key].content+'</pre></div>';
             return;
         }
     }
