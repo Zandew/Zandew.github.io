2da9a3658334e4098cd7f3d943a7acb06c23cb82
Andrew Xue <andewzxue@gmail.com>
2020-06-11 16:48:29 -0400

    f

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function about() {
 
 function solutions() {
     document.getElementById("info").innerHTML = '<input type="text" placeholder="Find a Problem" id="search-box" style="outline: none; width: 200px; height: 25px; display: inline-block; vertical-align: top;"><input type="image" src="/imgs/search.png" onclick="find()" style="width: 30px; height: 30px; outline: none; margin-left:5px">';
-    document.getElementById("code").innerHTML = '<h2 id="tip"> Enter a DMOJ or Codeforces problem code (e.g. ccc20s5 or 1234B) </h2>';
+    document.getElementById("code").innerHTML = '<h2 id="tip"> Enter a DMOJ or Codeforces problem code (e.g. ccc20s5 or 1234A) </h2>';
     document.getElementById("btn1").style.color = "gray";
     document.getElementById("btn2").style.color = "black";
 }
@@ -17,7 +17,7 @@ function find(){
     var prob = document.getElementById("search-box").value.trim().toLowerCase();
     for (var key in window.store){
         if (window.store[key].title.toLowerCase() == prob){
-            document.getElementById("code").innerHTML = "<p>"+window.store[key].content+"</p>";
+            document.getElementById("code").innerHTML = "<code>"+window.store[key].content+"</code>";
             return;
         }
     }
