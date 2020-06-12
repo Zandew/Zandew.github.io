ee45a0d4af0e07c970bbd9fbd5a00e79d6a285cf
Andrew Xue <andewzxue@gmail.com>
2020-06-12 10:24:02 -0400

    f

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function about() {
     document.getElementById("info").innerHTML = '<div id="about"> <img class="andrewxue" src="./andrewxue.png" alt="Andrew Xue" id="me"> <p id="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> </div>';
     document.getElementById("code").innerHTML = "";
@@ -15,17 +14,22 @@ function solutions() {
 
 function find(){
     var prob = document.getElementById("search-box").value.trim().toLowerCase();
-    var cont = window.localStorage.getItem(prob);
-    if (cont){
-        document.getElementById("code").innerHTML = '<div id="code-ctn"><pre class="prettyprint lang-cpp" style="float: left">'+cont+'</pre></div>';
-    }else {
-        document.getElementById("code").innerHTML = "<h1>NOT FOUND</h1>";
-    }
-    /*for (var key in window.store){
+    for (var key in window.store){
         if (window.store[key].title.toLowerCase() == prob){
             document.getElementById("code").innerHTML = '<div id="code-ctn"><pre class="prettyprint lang-cpp" style="float: left">'+window.store[key].content+'</pre></div>';
             return;
         }
     }
-    document.getElementById("code").innerHTML = "<h1>NOT FOUND</h1>";*/
+    document.getElementById("code").innerHTML = "<h1>NOT FOUND</h1>";
 }
+
+window.store = {
+    {% for post in site.posts %}
+    document.getElementById("storage").innerHTML += "<h1> H </h1>";
+    "{{ post.title }}": {
+        "title": "{{post.title | xml_escape}}",
+        "content": {{ post.content | jsonify}}
+    }
+    {% unless forloop.last %},{% endunless %}
+    {% endfor %}
+};
