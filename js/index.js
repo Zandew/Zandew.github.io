85ae3d8cdf8593f2f7a328a103a56fdc2ad3b41f
Andrew Xue <andewzxue@gmail.com>
2020-06-12 21:13:45 -0400

    H

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function about() {
-    document.getElementById("info").innerHTML = '<div id="about"> <img class="andrewxue" src="./andrewxue.png" alt="Andrew Xue" id="me"> <p id="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> </div>';
+    document.getElementById("info").innerHTML = '<div id="about"> <img class="andrewxue" src="./andrewxue.png" alt="Andrew Xue" id="me"> <p id="bio">'+{{me.content | jsonify}}+'</p> </div>';
     document.getElementById("code").innerHTML = "";
     document.getElementById("btn1").style.color = "black";
     document.getElementById("btn2").style.color = "gray";
