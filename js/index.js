13bbc047d812032a52a21795146d7472ee435f1b
Andrew Xue <andewzxue@gmail.com>
2020-08-10 20:43:55 -0400

    background

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function find(){
     var prob = document.getElementById("search-box").value.trim().toLowerCase();
     for (var key in window.store){
         if (window.store[key].title.toLowerCase() == prob){
-            document.getElementById("code").innerHTML = '<div id="code-ctn"><pre class="prettyprint lang-cpp">'+window.store[key].content+'</pre></div>';
+            document.getElementById("code").innerHTML = '<div id="code-ctn"><pre id="prettyprint" class="prettyprint lang-cpp">'+window.store[key].content+'</pre></div>';
             return;
         }
     }
