1126c41159ecdea45bbbdd6088e7b24df3cbfc05
Andrew Xue <andewzxue@gmail.com>
2020-08-28 20:07:14 -0400

    ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 function move(index) {
-    console.log("MOVE TO "+index);
     document.getElementById("main").style.transform = `translateX(calc(-100%/3*${index}))`;
     document.getElementById("btn"+index).style.color = "black";
     document.getElementById("btn"+(index+1)%3).style.color = "gray";
@@ -21,6 +20,80 @@ function find(){
     document.getElementById("error").innerHTML = "No File Found.";
 }
 
-window.onbeforeunload = function(event) {   
+function showText(idx) {
+    if (idx==0) {
+        document.getElementById('about-main').innerHTML = education;
+    }else if (idx==1) {
+        document.getElementById('about-main').innerHTML = programming;
+    }else if (idx==2) {
+        document.getElementById('about-main').innerHTML = hobbies;
+    }else if (idx==3) {
+        document.getElementById('about-main').innerHTML = accomplishments;
+    }
+}
+
+window.onbeforeunload = () => {   
     document.activeElement.blur();  
 };
+
+document.onkeydown = function (t) {
+    if(t.which == 9){
+        return false;
+    }
+}
+
+let iconName = ['education', 'programming', 'hobbies', 'accomplishments'];
+let aboutIcons = ['/imgs/education.png', '/imgs/programming.png', '/imgs/hobbies.png', '/imgs/accomplishments.png'];
+let aboutLightIcons = ['/imgs/educationLight.png', '/imgs/programmingLight.png', '/imgs/hobbiesLight.png', '/imgs/accomplishmentsLight.png'];
+
+function push(positions) {
+    let cx = window.innerWidth/2;
+    let cy = 125;
+    for (let i=0; i<4; i++){
+        let dx = positions[i].x*window.innerWidth*3/100-cx;
+        let dy = positions[i].y-cy;
+        setTimeout(() => {
+            document.getElementById(`about-item${i}`).style.transform = `translate(${dx}px, ${dy}px)`;
+        }, i*500);
+    }
+}
+
+window.onload = () => {
+    let positions = [];
+    let rad = 25;
+    let radBig = 175;
+    let eps = 75;
+    let lim = 300;
+    console.log(window.innerWidth);
+    for (let i=0; i<4; i++){
+        let farEnough = false;
+        console.log(i);
+        console.log(positions);
+        while (!farEnough) {
+            let x = Math.floor(Math.random() * 20)+6.5;
+            let y = Math.floor(Math.random() * 350);  
+            
+            farEnough = true;  
+            console.log(i);
+            for (let j=0; j<i; j++){
+                let dist = Math.sqrt(Math.pow(x*window.innerWidth*3/100-positions[j].x*window.innerWidth*3/100, 2)+Math.pow(y-positions[j].y, 2));
+                if (dist<rad*2+eps) {
+                    farEnough = false;
+                }
+            }
+            let dist = Math.sqrt(Math.pow(x*window.innerWidth*3/100-window.innerWidth/2, 2)+Math.pow(y-radBig, 2));
+            if (farEnough && dist > rad+radBig+eps && dist < lim) {
+                positions.push({ x, y });
+            }else farEnough = false;
+        }
+    } 
+    document.getElementById('about').innerHTML = `<div id="about-main" class="about-main">${defaultText}</div>`;
+    for (let i=0; i<4; i++){
+        document.getElementById('about').innerHTML += `
+            <div id="about-item${i}" class="about-item" onclick="showText(${i})">
+                <img id="${iconName[i]}" class="about-icon"></img>
+            </div>
+        `;
+    }
+    setTimeout(() => push(positions), 0);
+}
--- js/text.js
+++ js/text.js
@@ -1,20 +1,35 @@
-let aboutText = `
-    Hello, I'm Andrew Xue, a graduate from Earl Haig SS and also an incoming Computer Science student at the University of Waterloo.<br>
-    <br>I enjoy coding and am a (now retired) competitive programmer. Not that I\'ve had much of a career,
-     but I peaked in 2020 when I scored 69 on the Canadian Computing Competition and qualified for the Candian Computing Olympiad. 
-     Despite my decent score on the CCC, I only managed to come out with a bronze medal due to my subpar performance at the CCO.<br>
-     <br>Some of my hobbies include playing sports such as badminton and chess, binging anime and manga, and working on side projects.<br>
-     <br> While browsing through my DMOJ solutions, please note that these programs were written solely for the purpose of AC\'ing the problem, 
-     and therefore may lack proper formatting and comments. If you can\'t find a problem when I\'ve clearly solved it on DMOJ, 
-     it means I have been lazy and haven\'t uploaded it yet.;
+let defaultText = `
+    <h1>Hello, I'm Andrew Xue</h1>
 `
+let education = `
+    I'm a computer science student at the University of Waterloo. I am currently looking for 2021 spring internships for my 1C term. I graduate in 2025.
+`
+
+let programming = `
+    My programming knowledge largely consists of algorithms and data structures as I was mainly a competitive programmer, but I am looking to expand my horizons 
+    by delving into web development and software.
+`
+
+let hobbies = `
+    In my spare time, I enjoy playing sports (mainly badminton and chess), binging anime and manga, and working on side projects.    
+`
+
+let accomplishments = `
+    <div>
+        <h3> My Accomplishments </h3>
+        <ul> 
+            <li> CCO 2020 bronze medalist </li>
+            <li> ... thats pretty much it (for now!) </li>
+        </ul>
+    </div>
+`
+
 let oraText = 'A fully functional music player and playlist builder with ability to recommend songs based on user\'s mood. MacOS app built using Swift.';
 
 let pwText = 'A web app that calculates the \'worthiness\' of a present based on parameters specified by the user. Built using Node.js and Javascript.';
 
 let dpText = 'My implementations of simple neural networks in Python, vectorized using NumPy. Trained and tested for 80% accuracy on binary classification datasets (e.g cat or not).';
 
-document.getElementById("bio").innerHTML = aboutText;
 document.getElementById("oraText1").innerHTML = document.getElementById("oraText2").innerHTML = oraText;
 document.getElementById("pwText1").innerHTML = document.getElementById("pwText2").innerHTML = pwText;
 document.getElementById("dlText1").innerHTML = document.getElementById("dlText2").innerHTML = dpText;
